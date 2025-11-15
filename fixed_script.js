efcd2c652364499401ec8570c16f40e9f717c769
ilaproskin354-afk <ilaproskin354@gmail.com>
2025-11-16 03:45:40 +0700

    Add files via upload

---
+++ fixed_script.js
@@ -0,0 +1,10 @@
+// Fixed script placeholder
+const $ = id => document.getElementById(id);
+
+function saveState(){}
+function refreshUI(){}
+
+document.addEventListener("DOMContentLoaded", ()=>{
+  saveState();
+  refreshUI();
+});
